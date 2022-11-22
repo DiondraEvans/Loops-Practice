1ccaeaad50e034fe76254f833923d16dfbba40dc
Diondra <98505341+Diondraj@users.noreply.github.com>
2022-11-21 21:29:39 -0500

    made first three for loops

---
+++ jsloops.js
@@ -0,0 +1,16 @@
+for (i = 0; i <= 15; i++){
+    console.log(i);
+}
+console.log (" ");
+
+for(i = 15; i >= 0; i--){
+    console.log(i);
+}
+console.log(" ");
+
+let laps = 0;
+for( i = 0; i < 3; i++){
+    console.log("that’s another lap!");
+    laps += 1;
+}
+console.log(laps);
